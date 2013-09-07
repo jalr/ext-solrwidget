3d60b27da3a822e3cde4ac9c28d44d5625608423
Claus Due <claus@namelesscoder.net>
2013-09-07 23:23:42 +0200

    [TASK] Initialise repository

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "solrwidget".
+ *
+ * Auto generated 09-07-2013 00:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Solr Fluid Widget',
+	'description' => 'A Fluid Widget for use with EXT:solr - can be used as ViewHelper or as standalone plugin. Uses AJAX for searching and JS-filling of a Fluid-based HTML skeleton for displaying results.',
+	'category' => 'misc',
+	'author' => 'Claus Due',
+	'author_email' => 'claus@namelesscoder.net',
+	'author_company' => 'NamelessCoder',
+	'shy' => '',
+	'dependencies' => 'cms,extbase,fluid,solr',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-0.0.0',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
