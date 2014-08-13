6849c99fb73fd703ec4140cf4e50671280ad4ef9
Phuong Doan <phuong.doan@dkd.de>
2014-08-13 14:58:32 +0200

    Remove buildSolrQuery method, modify querySolrServer and searchAction methods

--- Classes/Controller/WidgetController.php
+++ Classes/Controller/WidgetController.php
@@ -31,9 +31,27 @@ class Tx_Solrwidget_Controller_WidgetController extends Tx_Fluid_Core_Widget_Abs
 	 * @return void
 	 */
 	public function searchAction($query) {
-		$solrQuery = $this->buildSolrQuery($query);
-		$results = $this->querySolrServer($solrQuery);
-		$json = json_encode($results, JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS);
+		$providers = $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['solrwidget']['queryProviders'];
+		$results = array();
+		foreach ($providers as $providerClassName) {
+			/** @var Tx_Solrwidget_QueryProvider_QueryProviderInterface $provider */
+			$provider = $this->objectManager->get($providerClassName);
+			$providedQueryOrNull = $provider->processQuery($query, TRUE === isset($results[0]) ? $results[0] : NULL);
+			if (NULL === $providedQueryOrNull) {
+				continue;
+			}
+			array_push($results, array(
+				'title' => $provider->getTitle($query, TRUE === isset($queries[0]) ? $queries[0] : NULL),
+				'query' => $providedQueryOrNull,
+				'results' => $this->querySolrServer($providedQueryOrNull)
+			));
+		}
+		if (1 === count($results)) {
+			$candidate = $results[0]['results'];
+		} else {
+			$candidate = $results;
+		}
+		$json = json_encode($candidate, JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS);
 		header('Content-type: text/json');
 		header('Content-length: ' . strlen($json));
 		echo $json;
@@ -41,26 +59,19 @@ class Tx_Solrwidget_Controller_WidgetController extends Tx_Fluid_Core_Widget_Abs
 	}
 
 	/**
-	 * @param string $queryString
-	 * @return tx_solr_Query
-	 */
-	protected function buildSolrQuery($queryString) {
-		/** @var tx_solr_Query $query */
-		$query = $this->objectManager->get('tx_solr_Query', $queryString);
-		$query->setQueryField('title', 1.0);
-		$query->addReturnField('title');
-		$query->setFieldList(array('title', 'url', 'teaser', 'score'));
-		return $query;
-	}
-
-	/**
 	 * @param tx_solr_Query $query
 	 * @return mixed
 	 */
 	protected function querySolrServer(tx_solr_Query $query) {
-		$this->searcher->renderCommand('search', array());
+		$queryString = $query->getQueryString();
 		$this->searcher->getSearch()->search($query);
 		$response = $this->searcher->getSearch()->getResponse();
+		if (1 > $response->response->numFound && 0 < $response->spellcheck->suggestions->{$queryString}->numFound) {
+			$firstSuggestedQueryString = $response->spellcheck->suggestions->{$queryString}->suggestion[0];
+			$query->setQueryString($firstSuggestedQueryString);
+			$query->useRawQueryString(TRUE);
+			$response = $this->searcher->getSearch()->search($query);
+		}
 		return Tx_Solrwidget_Utility_SolrResultFormatter::format($response);
 	}
 
