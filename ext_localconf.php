16ac7f49911fcbb381658df8e2e55ea205cd1a1d
Phuong Doan <phuong.doan@dkd.de>
2014-08-14 08:56:53 +0200

    Register hook for queryProvider

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,9 @@ Tx_Extbase_Utility_Extension::configurePlugin($_EXTKEY, 'Widget',
 	array('Plugin' => 'plugin'),
 	array('Plugin' => 'plugin')
 );
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['solrwidget'] = array(
+	'queryProviders' => array(
+		'Tx_Solrwidget_QueryProvider_InitialQueryProvider'
+	)
+);
